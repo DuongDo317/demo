9a3d19c6881dbc3dd3a4165c18a6c5214486b7b5
duongdo <duongdo317@gmail.com>
2019-09-28 15:22:34 +0700

    add rspec model

--- spec/models/user_history_spec.rb
+++ spec/models/user_history_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe UserHistory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "UserHistory associations" do
+    it { is_expected.to belong_to(:user) }
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "User associations" do
+    it { is_expected.to have_many(:user_infos) }
+    it { is_expected.to have_many(:user_histories) }
+  end
+
+  it { should enumerize(:status).in(deactivate: 0, active: 1) }
 end
